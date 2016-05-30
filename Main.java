c43f502f310ce857b01f7f69fa200d358ebe7974
Scott Banyard <scottbanyard1@yahoo.co.uk>
2016-05-30 12:22:02 +0100

    resizing

--- Main.java
+++ Main.java
@@ -9,28 +9,28 @@ import java.awt.event.ActionListener;
 import java.io.IOException;
 
 
-public class Menu extends JFrame {
+public class Main extends JFrame {
 
-	public Menu() throws IOException {
-		
+	public Main() throws IOException {
+
 		JFrame frame = new JFrame("French Test");
-		
+
 		frame.setSize(800, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		JPanel panel = new JPanel(new GridBagLayout());
-		
+
 		JLabel lbl = new JLabel("French Test");
 		lbl.setFont(new Font("Courier New", Font.BOLD, 50));
-		
-		
+
+
 		/* adds background image of french flag and frame adds the background
 		setLayout(new BorderLayout());
 		JLabel background = new JLabel(new ImageIcon("/Users/Scott/Documents/Java/workspace/FrenchTest/src/flag.png"));
 		background.setLayout(new FlowLayout());
 		frame.add(background);
 		*/
-		
+
 		JButton b1 = new JButton("Months");
 		b1.setFont(new Font("Courier New", Font.ITALIC, 20));
     	b1.addActionListener(new ActionListener() {
@@ -43,7 +43,7 @@ public class Menu extends JFrame {
 					}
      			}
      		});
-    	
+
     	JButton b2 = new JButton("Numbers");
     	b2.setFont(new Font("Courier New", Font.ITALIC, 20));
     	b2.addActionListener(new ActionListener() {
@@ -56,7 +56,7 @@ public class Menu extends JFrame {
 					}
      			}
      		});
-    	
+
     	JButton b3 = new JButton("Colours");
     	b3.setFont(new Font("Courier New", Font.ITALIC, 20));
     	b3.addActionListener(new ActionListener() {
@@ -69,50 +69,50 @@ public class Menu extends JFrame {
 					}
      			}
      		});
-		
+
     	JButton b4 = new JButton("Quit");
     	b4.setFont(new Font("Courier New", Font.ITALIC, 20));
     	b4.addActionListener(new ActionListener() {
-    		public void actionPerformed(ActionEvent e) 
+    		public void actionPerformed(ActionEvent e)
      		{
      			System.exit(0);
      		}
      	});
-    	
+
     	// creates a grid
     	GridBagConstraints c = new GridBagConstraints();
-    	
+
     	// insets cause space of 10 pixels between buttons
     	c.insets = new Insets(10, 10, 10, 10);
     	c.gridx = 0;
     	c.gridy = 1;
     	panel.add(lbl);
     	panel.add(b1, c);
-    	
+
     	c.gridx = 0;
     	c.gridy = 2;
     	panel.add(b2, c);
-    	
+
     	c.gridx = 0;
     	c.gridy = 3;
     	panel.add(b3, c);
-    	
+
     	c.gridx = 0;
     	c.gridy = 4;
     	panel.add(b4, c);
-    	
+
     	frame.add(panel, BorderLayout.CENTER);
-    	
-		
-    	
+
+
+
 		// sets frame centrally in screen
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
 		frame.setVisible(true);
-		
+
 	}
-	
+
 	public static void main(String [] args) throws IOException {
-		Menu menu = new Menu();
+		Main menu = new Main();
 	}
 }
--- MonthsTest.java
+++ MonthsTest.java
@@ -17,18 +17,18 @@ public class MonthsTest extends JFrame {
 	static int randomNumber = 0;
 	public static String randomQuestion = "";
 	public static ArrayList<String> usedQuestions = new ArrayList<String>();
-	
+
 	public static void main(JFrame frame) throws IOException {
 		test(frame);
 	}
-	
+
 	public static void test(JFrame frame) {
 		// removes all content on frame passed in (clears frame)
 		frame.getContentPane().removeAll();
 		frame.getContentPane().repaint();
-		
+
 		JPanel panel = new JPanel(new GridBagLayout());
-		
+
 		HashMap<String, String> questions = new HashMap<String, String>();
 		questions.put("What is 'January'?", "janvier");
 		questions.put("What is 'February'?", "fevrier");
@@ -42,39 +42,39 @@ public class MonthsTest extends JFrame {
 		questions.put("What is 'October'?", "octobre");
 		questions.put("What is 'November'?", "novembre");
 		questions.put("What is 'December'?", "decembre");
-		
+
 		// ends test after 5 questions
 		if (questionCount != 5) {
-			
+
 			Random randomizer = new Random();
-			
+
 			// makes array with keys of hashmap (the questions)
 			ArrayList<String> keysAsArray = new ArrayList<String>(questions.keySet());
 			// gets a random question from keyset
 			randomNumber = randomizer.nextInt(keysAsArray.size());
 			randomQuestion = keysAsArray.get(randomizer.nextInt(keysAsArray.size()));
 
-			
+
 			// makes sure not using question that has already been used
 			while (usedQuestions.contains(randomQuestion)) {
 				randomNumber = randomizer.nextInt(keysAsArray.size());
 				randomQuestion = keysAsArray.get(randomNumber);
 			}
 			usedQuestions.add(randomQuestion);
-			
+
 			// changes label to question
 			JLabel lbl = new JLabel(randomQuestion);
 			lbl.setFont(new Font("Courier New", Font.BOLD, 50));
-			
+
 			// sets up textfield
 			JTextField textfield = new JTextField(15);
 			textfield.setFont(new Font("Courier New", Font.LAYOUT_LEFT_TO_RIGHT, 30));
-			
+
 			// sets up submit button
 			JButton submitButton = new JButton("Submit");
 			submitButton.setFont(new Font("Courier New", Font.ITALIC, 20));
 	    	submitButton.addActionListener(new ActionListener() {
-	    		public void actionPerformed(ActionEvent e) 
+	    		public void actionPerformed(ActionEvent e)
 	     		{
 	    			// gets answer given
 	    			String textfieldAnswer = textfield.getText();
@@ -89,76 +89,76 @@ public class MonthsTest extends JFrame {
 	     			test(frame);
 	     		}
 	     	});
-			
-			
+
+
 	    	// creates a grid layout
 	    	GridBagConstraints c = new GridBagConstraints();
-	    	
+
 	    	// insets cause space of 10 pixels between buttons
 	    	c.insets = new Insets(10, 10, 10, 10);
 	    	c.gridx = 0;
 	    	c.gridy = 1;
 	    	panel.add(lbl, c);
-	    	
+
 	    	c.gridx = 0;
 	    	c.gridy = 2;
 	    	panel.add(textfield, c);
-	    	
+
 	    	c.gridx = 0;
 	    	c.gridy = 3;
 	    	panel.add(submitButton, c);
-    	
+
 	    	frame.add(panel, BorderLayout.CENTER);
-	    	
+
 			frame.setVisible(true);
-			
+
 		} else {
 			// END OF TEST - RETURNS SCORE
-			
+
 			// clears window
 			frame.getContentPane().removeAll();
 			frame.getContentPane().repaint();
-			
+
 			// sets up button to take you back to the main menu
 			JButton menuButton = new JButton("Back to Menu");
 			menuButton.setFont(new Font("Courier New", Font.ITALIC, 20));
 	    	menuButton.addActionListener(new ActionListener() {
-	    		public void actionPerformed(ActionEvent e) 
+	    		public void actionPerformed(ActionEvent e)
 	     		{
      				try {
      					// closes current window and opens main menu
      					frame.dispose();
      					// resets question count
      					questionCount = 0;
-						Menu.main(null);
+						Main.main(null);
 					} catch (IOException e1) {
 						e1.printStackTrace();
 					}
 	     		}
 	     	});
-			
+
 			// sets up label showing result of test
 			JLabel result = new JLabel("That's the end of the test. You managed to score " + score + " out of 5.");
 			result.setFont(new Font("Courier New", Font.BOLD, 20));
-			
+
 	    	// creates a grid layout
 	    	GridBagConstraints c = new GridBagConstraints();
-	    	
+
 	    	c.insets = new Insets(10, 10, 10, 10);
 	    	c.gridx = 0;
 	    	c.gridy = 1;
 	    	panel.add(result, c);
-	    	
+
 	    	c.gridx = 0;
 	    	c.gridy = 2;
 	    	panel.add(menuButton, c);
-	    	
+
 	    	frame.add(panel, BorderLayout.CENTER);
 	    	frame.setVisible(true);
-			
+
 		}
-		
+
 	}
-	
-	
+
+
 }
